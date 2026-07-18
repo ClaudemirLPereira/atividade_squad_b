289fb57ad2201d3b3667813f34befb244e66e810
Claudemir Lopes Pereira <pereiraclaudemir@outlook.com>
2026-07-17 21:05:19 -0300

    Adicionado o produto 'MONITOR'

--- squad_b.py
+++ squad_b.py
@@ -2,3 +2,9 @@ cadastro_produtos = [
     {"nome": "Mouse", "preco": 45.0},
     {"nome": "Teclado", "preco": 89.0},
 ]
+
+cadastro_produtos.append({"nome": "Monitor", "preco": 350.0})
+
+for item in cadastro_produtos:
+    print(f"Produto: {item['nome']}, Preço: R${item['preco']:.2f}") 
+    
