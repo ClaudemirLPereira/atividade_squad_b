037c6198e2da7dcf4ca2f683be6bd7e39e999449
Eduardo <joneves90@gmail.com>
2026-07-17 21:32:45 -0300

    adicionando produto do Eduardo

--- squad_b.py
+++ squad_b.py
@@ -5,7 +5,7 @@ cadastro_produtos = [
 
 cadastro_produtos.append({"nome": "Monitor", "preco": 350.0})
 cadastro_produtos.append({"nome": "Headphone", "preco": 500.0})
-
+cadastro_produtos.append({"nome": "mouse-pad", "preco": 90.0 })
 for item in cadastro_produtos:
     print(f"Produto: {item['nome']}, Preço: R$ {item['preco']:.2f}") 
     
